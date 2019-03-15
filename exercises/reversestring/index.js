208b1a27630788fb66732ebd4c8ce8408746b26e
Russ Storms <rstorms90@gmail.com>
2019-03-15 10:40:30 -0600

    reverseStr

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,6 +6,15 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+// function reverse(str) {
+//   return str.split('').reverse().join('')
+// }
+
+function reverse(str) {
+  debugger
+  return str.split('').reduce((rev, char) => char + rev, '')
+}
+
+reverse('asdf')
 
 module.exports = reverse;
